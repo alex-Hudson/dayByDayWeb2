90aac8970be7a3a7852c21738a7d08ef4a16665a
Alex Hudson <hudsonj.alex@gmail.com>
2020-03-28 10:47:54 +0000

    Allowed user to only see todays reading

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,26 +14,64 @@ class App extends Component {
         title: "",
         description: "",
         completed: false,
-        pub_date: ""
+        reading_date: ""
       },
       todoList: []
     };
   }
+
   componentDidMount() {
     this.refreshList();
   }
+
+  /**
+   * Gets all ToDo Items in database
+   */
   refreshList = () => {
     axios
       .get("http://localhost:8000/api/todos/")
       .then(res => this.setState({ todoList: res.data }))
+      .then(() => {
+        this.setState({ todoList: this.getTodaysReading() });
+      })
       .catch(err => console.log(err));
   };
+
+  /**
+   * Returns readings recording for today
+   */
+  getTodaysReading = () => {
+    const toDoList = this.state.todoList;
+    const isToday = someDate => {
+      const today = new Date();
+      return (
+        someDate.getDate() === today.getDate() &&
+        someDate.getMonth() === today.getMonth() &&
+        someDate.getFullYear() === today.getFullYear()
+      );
+    };
+    console.log(toDoList);
+    const filteredToDo = [];
+    toDoList.forEach(toDoItem => {
+      if (isToday(new Date(toDoItem.reading_date))) filteredToDo.push(toDoItem);
+    });
+    console.log(filteredToDo);
+    return filteredToDo;
+  };
+
+  /**
+   * Displays completed items
+   */
   displayCompleted = status => {
     if (status) {
       return this.setState({ viewCompleted: true });
     }
     return this.setState({ viewCompleted: false });
   };
+
+  /**
+   * Creates button to switch between completed and incomplete items
+   */
   renderTabList = () => {
     return (
       <div className="my-5 tab-list">
@@ -52,6 +90,10 @@ class App extends Component {
       </div>
     );
   };
+
+  /**
+   * Displays list of items
+   */
   renderItems = () => {
     const { viewCompleted } = this.state;
     const newItems = this.state.todoList.filter(
@@ -69,9 +111,16 @@ class App extends Component {
           title={item.description}
         >
           {item.title}
-          {item.pub_date}
+          {this.formatDate(item.reading_date)}
         </span>
         <span>
+          <button
+            onClick={() => this.viewReading(item)}
+            className="btn btn-secondary mr-2"
+          >
+            View This Reading{" "}
+          </button>
+
           <button
             onClick={() => this.editItem(item)}
             className="btn btn-secondary mr-2"
@@ -89,9 +138,17 @@ class App extends Component {
       </li>
     ));
   };
+
+  /**
+   *
+   */
   toggle = () => {
     this.setState({ modal: !this.state.modal });
   };
+
+  /**
+   * Handles sumbit when creating new item
+   */
   handleSubmit = item => {
     this.toggle();
     if (item.id) {
@@ -104,18 +161,39 @@ class App extends Component {
       .post("http://localhost:8000/api/todos/", item)
       .then(res => this.refreshList());
   };
+
+  /**
+   * Deletes item
+   */
   handleDelete = item => {
     axios
       .delete(`http://localhost:8000/api/todos/${item.id}`)
       .then(res => this.refreshList());
   };
+
+  /**
+   * Creates item with null fields
+   */
   createItem = () => {
-    const item = { title: "", description: "", completed: false, pub_date: "" };
+    const item = {
+      title: "",
+      description: "",
+      completed: false,
+      reading_date: ""
+    };
     this.setState({ activeItem: item, modal: !this.state.modal });
   };
+
+  /**
+   * Edit item
+   */
   editItem = item => {
     this.setState({ activeItem: item, modal: !this.state.modal });
   };
+
+  /**
+   * Renders main page
+   */
   render() {
     return (
       <main className="content">
@@ -148,5 +226,27 @@ class App extends Component {
       </main>
     );
   }
+
+  /**
+   * Formats date from server into human readable
+   * @param {string} dateString
+   */
+  formatDate(dateString) {
+    const d = new Date(dateString);
+    const dtf = new Intl.DateTimeFormat("en", {
+      year: "numeric",
+      month: "short",
+      day: "2-digit"
+    });
+    const [{ value: mo }, , { value: da }, , { value: ye }] = dtf.formatToParts(
+      d
+    );
+    return `${da}-${mo}-${ye}`;
+  }
+
+  /**
+   * Stub
+   */
+  viewReading() {}
 }
 export default App;
