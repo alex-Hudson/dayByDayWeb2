6a3f2f4ee4f09d33ff693d11f05acae69d3b7598
Alex Hudson <hudsonj.alex@gmail.com>
2020-04-18 16:02:57 +0100

    Changed database to postgres

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -97,8 +97,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'DayByDayWebsite',
+        'USER': 'postgres',
+        'PASSWORD': 'Googleyahoo1',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
